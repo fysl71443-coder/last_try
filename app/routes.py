0872432707ab9290df34526508c46662d8487e7c
fysl71443-coder <fysl71443@gmail.com>
2025-09-20 20:19:34 +0300

    Fix auth wiring: use blueprint endpoints in index (main.login/main.logout); add logout route; call login_user on successful login

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
 from app import db
 from app.models import User
 
@@ -13,10 +14,16 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        # تحقق من المستخدم
         user = User.query.filter_by(username=username).first()
         if user and user.check_password(password):
+            login_user(user)
             return redirect(url_for('main.home'))
         else:
             flash('خطأ في اسم المستخدم أو كلمة المرور')
     return render_template('login.html')
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
